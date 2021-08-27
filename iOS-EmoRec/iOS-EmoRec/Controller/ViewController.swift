89128c643a67a0e6b515deaac52ce095ba57fbe8
feldblume5263 <feldblume5263@gmail.com>
2021-08-27 13:32:14 +0900

    feat: #1 set up Xcode config complete set up Xcode config.
    with StoryBoard UI
    with Swift
    with CoreData
    
    without CloudKit
    without Testfile

---
+++ iOS-EmoRec/iOS-EmoRec/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-EmoRec
+//
+//  Created by Junhong Park on 2021/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
