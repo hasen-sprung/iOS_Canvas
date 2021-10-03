2af9252333abec85de300cd6c58c82ec5b533fc8
Jaeyoung Lee <ejei0g@gmail.com>
2021-10-03 20:16:31 +0900

    feat: #25 gradient color 가져오기 - 하단 색상과 상단 색상(변하는) 이 고정되어 있을 때, - figure수치를 통해 중간값 가져오기 (percentage)

--- project-Emotion/project-Emotion/Constants/Colors.swift
+++ project-Emotion/project-Emotion/Constants/Colors.swift
@@ -11,3 +11,6 @@ let defaultBackgroundColorBottom = UIColor(red: 1.000, green: 0.357, blue: 0.525
 
 let sampleBlueTop = UIColor(red: 0.118, green: 0.376, blue: 0.682, alpha: 1.00)
 let sampleBlueBottom = UIColor(red: 0.569, green: 0.824, blue: 0.941, alpha: 1.00)
+
+let gaugeViewColorTop: UIColor = #colorLiteral(red: 0.6965150237, green: 0.698797524, blue: 1, alpha: 1)
+let gaugeViewColorBottom: UIColor = #colorLiteral(red: 1, green: 0.7658135295, blue: 0.7398825288, alpha: 1)
