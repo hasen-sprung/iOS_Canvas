925f2a4ddcdc3ed90ce8555efe101992182c070b
feldblume5263 <feldblume5263@gmail.com>
2021-10-11 17:36:35 +0900

    feat: #56 date class 생성 date class를 통해, current date를 조작.

--- project-Emotion/project-Emotion/Controller/MainViewController.swift
+++ project-Emotion/project-Emotion/Controller/MainViewController.swift
@@ -5,22 +5,125 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var addRecordButton: UIButton!
     @IBOutlet weak var gotoSettingButton: UIButton!
+    
+    @IBOutlet weak var dateForwardButton: UIButton!
+    @IBOutlet weak var dateBackwardButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var selectDateView: UIView!
+    @IBOutlet weak var selectDayButton: UIButton!
+    @IBOutlet weak var selectWeekButton: UIButton!
+    @IBOutlet weak var selectMonthButton: UIButton!
+    
     let userDefaults = UserDefaults.standard
     
     var theme = ThemeManager.shared.getThemeInstance()
+    let dateManager = DateManager.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         getUserDefault()
         setAddRecordButton()
+        setDateButtons()
+        setDateLabel()
+        setSelectDateView()
+        
     }
     override func viewWillAppear(_ animated: Bool) {
         theme = ThemeManager.shared.getThemeInstance()
         self.view.backgroundColor = theme.getColor().view.main
     }
     
+    private func setSelectDateView() {
+        
+        selectDateView.backgroundColor = .clear
+        selectDateView.frame.size = CGSize(width: view.frame.width * 0.7, height: 50)
+        selectDateView.center = CGPoint(x: view.frame.width / 2, y: view.frame.height * 0.15)
+        
+        selectDayButton.frame.size = CGSize(width: selectDateView.frame.width * 0.3, height: selectDateView.frame.height * 0.8)
+        selectDayButton.center = CGPoint(x: selectDateView.frame.width * 0.25, y: selectDateView.frame.height / 2)
+        selectDayButton.setTitle("Day", for: .normal)
+        selectDayButton.addTarget(self, action: #selector(changeDateModeToDate), for: .touchUpInside)
+        
+        selectWeekButton.frame.size = CGSize(width: selectDateView.frame.width * 0.3, height: selectDateView.frame.height * 0.8)
+        selectWeekButton.center = CGPoint(x: selectDateView.frame.width * 0.5, y: selectDateView.frame.height / 2)
+        selectWeekButton.setTitle("Week", for: .normal)
+        selectWeekButton.addTarget(self, action: #selector(changeDateModeToWeek), for: .touchUpInside)
+        
+        selectMonthButton.frame.size = CGSize(width: selectDateView.frame.width * 0.3, height: selectDateView.frame.height * 0.8)
+        selectMonthButton.center = CGPoint(x: selectDateView.frame.width * 0.75, y: selectDateView.frame.height / 2)
+        selectMonthButton.setTitle("Month", for: .normal)
+        selectMonthButton.addTarget(self, action: #selector(changeDateModeToMonth), for: .touchUpInside)
+    }
+    
+    @objc func changeDateModeToDate() {
+        
+        dateManager.setDateMode(newMode: 0)
+        dateLabel.text = dateManager.getCurrentDateString()
+    }
+    
+    @objc func changeDateModeToWeek() {
+        
+        dateManager.setDateMode(newMode: 1)
+        dateLabel.text = dateManager.getCurrentDateString()
+    }
+    
+    @objc func changeDateModeToMonth() {
+        
+        dateManager.setDateMode(newMode: 2)
+        dateLabel.text = dateManager.getCurrentDateString()
+    }
+    
+    private func setDateLabel() {
+        
+        dateLabel.frame.size = CGSize(width: 300, height: 50)
+        dateLabel.center = CGPoint(x: view.frame.width * 0.5, y: view.frame.height * 0.2)
+        dateLabel.text = dateManager.getCurrentDateString()
+        dateLabel.textAlignment = .center
+    }
+    
+    private func setDateButtons() {
+        
+        setDateForwardButton()
+        setDateBackwardButton()
+    }
+    
+    private func setDateBackwardButton() {
+        
+        dateBackwardButton.frame.size = CGSize(width: 50, height: 50)
+        dateBackwardButton.center = CGPoint(x: view.frame.width * 0.2, y: view.frame.height * 0.2)
+        dateBackwardButton.setImage(UIImage(systemName: "chevron.backward"), for: .normal)
+        dateBackwardButton.addTarget(self, action: #selector(dateBackwardButtonPressed), for: .touchUpInside)
+        dateBackwardButton.backgroundColor = .clear
+        dateBackwardButton.setTitle("", for: .normal)
+        
+    }
+    
+    private func setDateForwardButton() {
+        
+        dateForwardButton.frame.size = CGSize(width: 50, height: 50)
+        dateForwardButton.center = CGPoint(x: view.frame.width * 0.8, y: view.frame.height * 0.2)
+        dateForwardButton.setImage(UIImage(systemName: "chevron.forward"), for: .normal)
+        dateForwardButton.addTarget(self, action: #selector(dateForwardButtonPressed), for: .touchUpInside)
+        dateForwardButton.backgroundColor = .clear
+        dateForwardButton.setTitle("", for: .normal)
+    }
+    
+    @objc func dateForwardButtonPressed() {
+        
+        dateManager.moveDate(val: 1)
+        dateLabel.text = dateManager.getCurrentDateString()
+    }
+    
+    @objc func dateBackwardButtonPressed() {
+        
+        dateManager.moveDate(val: -1)
+        dateLabel.text = dateManager.getCurrentDateString()
+    }
+    
     func setAddRecordButton() {
+        
         let mainViewWidth = self.view.frame.width
         let mainViewHeight = self.view.frame.height
         let buttonSize = mainViewWidth * 0.2
