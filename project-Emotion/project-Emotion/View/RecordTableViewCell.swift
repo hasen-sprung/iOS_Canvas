e5731e641e1946ca4bc7556d724064999f4c18a8
feldblume5263 <feldblume5263@gmail.com>
2021-10-15 14:00:53 +0900

    feat: #71 xib 파일 생성 xib 파일 생성 후, 필요한 요소 추가함

---
+++ project-Emotion/project-Emotion/View/RecordTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecordTableViewCell.swift
+//  project-Emotion
+//
+//  Created by Junhong Park on 2021/10/15.
+//
+
+import UIKit
+
+class RecordTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
