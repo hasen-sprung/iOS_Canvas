f09c15f04c7e51d95f1270a969f771fc24d54811
feldblume5263 <feldblume5263@gmail.com>
2021-10-29 17:16:59 +0900

    feat: #86 set view background Layout
    set background layout randomly but no fully random

--- project-Emotion/project-Emotion/Controller/GaugeWaveViewController.swift
+++ project-Emotion/project-Emotion/Controller/GaugeWaveViewController.swift
@@ -6,24 +6,57 @@
 //
 
 import UIKit
+import Macaw
 
 class GaugeWaveViewController: UIViewController {
-
+    
+    private var backgroundShapes = [UIView]()
+    
+    private let theme = ThemeManager.shared.getThemeInstance()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setBackgroound()
+        
+    }
+
+    
+    private func setBackgroound() {
+        
+        setShapes()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func setShapes() {
+        
+        let centerX: [CGFloat] = [10.0, 30.0, 50.0, 70.0, 90.0]
+        let centerY: [CGFloat] = [10.0, 30.0, 50.0, 70.0, 90.0]
+        
+        for y in 0 ..< centerY.count {
+            
+            for x in 0 ..< centerX.count {
+                
+                let newWidth = CGFloat.random(in: 20 ... 40)
+                let newHeight = CGFloat.random(in: 20 ... 40)
+                
+                let newView = SVGView()
+                newView.frame.size = CGSize(width: newWidth, height: newHeight)
+                newView.center = CGPoint(x: getRatioLen(ratio: centerX[x], tag: 0) + getRatioLen(ratio: CGFloat.random(in: -3 ... 3), tag: 0),
+                                         y: getRatioLen(ratio: centerY[y], tag: 1) + getRatioLen(ratio: CGFloat.random(in: -3 ... 3), tag: 1))
+                newView.backgroundColor = .blue
+                backgroundShapes.append(newView)
+                view.addSubview(newView)
+            }
+        }
     }
-    */
-
+    
+    private func getRatioLen(ratio: CGFloat, tag: Int) -> CGFloat {
+        
+        if tag == 0 {
+            return (view.frame.width * ratio) / 100
+        } else {
+            return (view.frame.height * ratio) / 100
+        }
+    }
 }
